6f0b07e1c908ecc0e3a15f8d9728f61cdcf73e5a
Jinfeng Jiang <jeffjiang8@gmail.com>
2019-09-07 22:52:24 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -12,7 +12,7 @@ def load_library (file_path)
   
   emoticons.each do |key, value|
     new_hash['get_emoticon'][value[0]] = emoticons[key][1]
-    new_hash['get_meaningt'][]
+    new_hash['get_meaningt'][value[1]] = emoticons[key]
 end
 
 def get_japanese_emoticon
