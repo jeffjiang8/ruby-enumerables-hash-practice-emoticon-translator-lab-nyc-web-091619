3fdfb2ac2db4b539a21f200f4d6fca2e5a027f8c
Jinfeng Jiang <jeffjiang8@gmail.com>
2019-09-07 22:57:19 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -12,15 +12,19 @@ def load_library (file_path)
   
   emoticons.each do |key, value|
     new_hash['get_emoticon'][value[0]] = emoticons[key][1]
-    new_hash['get_meaningt'][value[1]] = key
+    new_hash['get_meaning'][value[1]] = key
   end 
   new_hash
 end
 
-def get_japanese_emoticon
+def get_japanese_emoticon(file_path, emoticon)
   # code goes here
+  result = load_library(file_path)['get_meaning'][emoticon]
+  result ? result:"Sorry, that emoticon was not found"
 end
 
-def get_english_meaning
+def get_english_meaning(file_path, emoticon)
   # code goes here
+  result = load_library(file_path)["get_meaning"][emoticon]
+  result ? result:"Sorry, that emoticon was not found"
 end
